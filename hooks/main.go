82c47118d51d38d43f2a2f3c996f5491a2acc383
Smyslov Maxim <maksim.smyslov@flant.com>
2025-06-10 16:46:45 +0300

    Readiness
    Signed-off-by: Smyslov Maxim <maksim.smyslov@flant.com>

--- hooks/main.go
+++ hooks/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log/slog"
+	"net/http"
 
 	_ "hook/https"
 
@@ -74,6 +77,42 @@ func parse_snap_version(version VersionInfoMetadata) string {
 	return string(version.Major) + "." + string(version.Minor) + "." + string(version.Patch)
 }
 
+func ReadinessFunc(_ context.Context, input *pkg.HookInput) error {
+	input.Logger.Info("start user logic for readiness probe")
+
+	c := input.DC.GetHTTPClient()
+
+	req, err := http.NewRequest(http.MethodGet, "http://127.0.0.1/", nil)
+	if err != nil {
+		return fmt.Errorf("create request: %w", err)
+	}
+
+	resp, err := c.Do(req)
+	if err != nil {
+		return fmt.Errorf("do request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("read response body: %w", err)
+	}
+
+	input.Logger.Debug("readiness probe done successfully", slog.Any("body", string(respBody)))
+
+	return nil
+}
+
 func main() {
-	app.Run()
+	readinessConfig := &app.ReadinessConfig{
+		ModuleName:        "echo-server",
+		IntervalInSeconds: 12,
+		ProbeFunc:         ReadinessFunc,
+	}
+
+	app.Run(app.WithReadiness(readinessConfig))
 }
